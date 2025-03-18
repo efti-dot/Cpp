5b7128c868f9ec98c76904c6c833d68d0a558c8a
efti-dot <eftih31@gmail.com>
2025-03-18 16:52:44 +0600

    Create phoneDialing.cpp

---
+++ phoneDialing.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int a;
+    cin>>a;
+
+    if(a == 61){
+        cout<<"Brasilia";
+    }
+    else if(a == 71){
+        cout<<"Salvador";
+    }
+    else if(a == 11){
+        cout<<"Sao Paulo";
+    }
+    else if(a == 21){
+        cout<<"Rio de Janeiro";
+    }
+    else if(a == 32){
+        cout<<"Juiz de Fora";
+    }
+    else if(a == 19){
+        cout<<"Campinas";
+    }
+    else if(a == 27){
+        cout<<"Vitoria";
+    }
+    else if(a == 31){
+        cout<<"Belo Horizonte";
+    }
+    else{
+        cout<<"DDD nao cadastrado";
+    }
+
+}
